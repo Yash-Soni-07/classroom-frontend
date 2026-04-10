ddc0bcd7133a74b874228326e265c8d1e32c867b
Yash-Soni-07 <yashsoni0701@gmail.com>
2026-04-11 02:38:14 +0530

    implement data provider

---
+++ src/constants/index.ts
@@ -0,0 +1,10 @@
+export const DEPARTMENTS = [
+    'Computer Science',
+    'Maths',
+    'English',
+];
+
+export const DEPARTMENT_OPTIONS = DEPARTMENTS.map((dept) => ({
+    value: dept,
+    label: dept,
+}))
